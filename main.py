ee6e3a93d5bd092b875821b3a7a3d37c8f049f3d
huytran2005 <huytran20062023@gmail.com>
2026-01-03 22:33:18 +0700

    feat(api): add restaurant dashboard management SSRMPWQCO-179

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from controllers.review_controller import router as review_router
+from controllers.customer_dashboard_controller import router as customer_dashboard_router
 
 from controllers.auth_controller import router as auth_router
 from controllers.restaurant_controller import router as restaurant_router
@@ -11,6 +12,10 @@ from controllers.qr_controller import router as qr_router
 from controllers.guest import router as guest_router
 from controllers.order_controller import router as order_router
 from controllers.points_controller import router as point_router
+from controllers.report_controller import router as report_router
+from controllers.menu_analytics_controller import router as menu_analytics_router
+from controllers.point_dashboard_controller import router as point_dashboard_router
+from controllers.review_dashboard_controller import router as review_dashboard_router
 
 from db.init_db import init_db
 
@@ -36,6 +41,11 @@ def create_app() -> FastAPI:
     app.include_router(order_router)
     app.include_router(review_router)
     app.include_router(point_router)
+    app.include_router(report_router)
+    app.include_router(menu_analytics_router)
+    app.include_router(customer_dashboard_router)
+    app.include_router(point_dashboard_router)
+    app.include_router(review_dashboard_router)
 
     return app
 
